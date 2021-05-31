63a78c9369b3d3aa334dffd95f02403865d15538
unknown <twujstarypijak420@gmail.com>
2021-05-31 13:47:04 +0200

    cala gra

---
+++ Assets/Scripts/BasketMovement.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasketMovement : MonoBehaviour
+{
+    private Rigidbody2D myBody;
+    
+    public float speed, xBound;
+
+    void Start()
+    {
+        myBody = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        float h = Input.GetAxisRaw("Horizontal");
+
+        if(h > 0)
+        
+            myBody.velocity = Vector2.right * speed;
+        else if(h < 0)
+        
+            myBody.velocity = Vector2.left * speed;
+        else 
+            myBody.velocity = Vector2.zero;
+
+        transform.position = new Vector2(Mathf.Clamp(transform.position.x, -xBound, xBound),
+        transform.position.y);
+    }
+}
